4e634e88085162fb58d2f9505f52a67c5d931543
Daksh Khatri <daksh.dk207@gmail.com>
2021-10-17 17:55:04 +0530

    changed header bg

---
+++ build/precache-manifest.6ed9dc79e93fb729e1918ed1c152f8b0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edcab9bc69c52d9d7b039162ea7f9699",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "9b1b339b362e7faec60c",
+    "url": "/keeper-app/static/js/2.b7a02d56.chunk.js"
+  },
+  {
+    "revision": "185a272f80eb23d861bc",
+    "url": "/keeper-app/static/js/main.b99f7d45.chunk.js"
+  },
+  {
+    "revision": "da401ec4583533870100",
+    "url": "/keeper-app/static/js/runtime-main.f8f1107a.js"
+  }
+]);
